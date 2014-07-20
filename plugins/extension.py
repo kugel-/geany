9e3ae679f7f2f194ba5a73e18f59e146eb05d95e
Thomas Martitz <kugel@rockbox.org>
2014-07-20 23:09:46 +0200

    Extend python test plugin to call GeanyGI APIs
    It will now create a dummy test document, and call save() and close() on it

--- plugins/extension.py
+++ plugins/extension.py
@@ -2,14 +2,18 @@ import gi
 
 from gi.repository import GObject
 from gi.repository import Geany
+from gi.repository import GeanyGI
 
 class PythonHelloPlugin2(GObject.Object, Geany.Plugin2):
     __gtype_name__ = 'PythonHelloPlugin2'
 
     methods = GObject.property(type = int, flags = GObject.PARAM_READABLE, default = Geany.Methods.CLEANUP)
+    doc = None
 
     def do_initialize(self):
-        print("PythonHelloPlugin.do_init\n")
+        self.doc = GeanyGI.Document.new_file("/tmp/foo.c",
+                            GeanyGI.Filetype.get_by_id(Geany.FiletypeID.C), "int")
+        self.doc.save(False)
 
     def do_version_check(self, abi):
         return 218
@@ -25,6 +29,7 @@ class PythonHelloPlugin2(GObject.Object, Geany.Plugin2):
 
     def do_cleanup(self):
         print("cleanup\n")
+        self.doc.close()
 
 def __init__():
     print("foo")
