b5c6365bb1377a92a17f521b54dcd1fdf0ba509d
Thomas Martitz <kugel@rockbox.org>
2014-09-23 07:25:13 +0200

    libpeas: fix compiler warning by including pyconfig.h separately.
    This avoids compiler warnings for e.g. _POSIX_C_SOURCE which is supposed
    to be compiled by the user and is actually defined by pyconfig.h. So these
    defines should be visibible before system headers.

--- libpeas-loaders/python/peas-plugin-loader-python.c
+++ libpeas-loaders/python/peas-plugin-loader-python.c
@@ -23,13 +23,10 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+#include <pyconfig.h>
 
 #include "peas-plugin-loader-python.h"
 
-/* _POSIX_C_SOURCE is defined in Python.h and in limits.h included by
- * glib-object.h, so we unset it here to avoid a warning. Yep, that's bad.
- */
-#undef _POSIX_C_SOURCE
 #include <pygobject.h>
 #include <Python.h>
 #include <signal.h>
