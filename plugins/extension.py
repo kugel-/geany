36242b69be14da9ac28abd66e8b4cff58dd76370
Thomas Martitz <kugel@rockbox.org>
2014-10-01 00:31:08 +0200

    extension.py: Append for some keybindings test.

--- plugins/extension.py
+++ plugins/extension.py
@@ -1,6 +1,8 @@
 import gi
 
 from gi.repository import GObject
+from gi.repository import Gtk
+from gi.repository import GLib
 from gi.repository import Geany
 from gi.repository import GeanyGI
 
@@ -9,11 +11,21 @@ class PythonHelloPlugin2(GObject.Object, Geany.Plugin2):
 
     methods = GObject.property(type = int, flags = GObject.PARAM_READABLE, default = Geany.Methods.CLEANUP)
     doc = None
+    keys = None
+
+    def on_test_activate(self, key, key_id):
+        print("on_test_activate")
 
     def do_initialize(self):
         self.doc = GeanyGI.Document.new_file("/tmp/foo.c",
                             GeanyGI.Filetype.get_by_id(Geany.FiletypeID.C), "int")
         self.doc.save(False)
+        self.keys = GeanyGI.KeyGroup.new("h_p", "Hello Python", 1)
+        print(self.keys)
+        #~ key = GeanyGI.KeyBinding.new("test", "Test", self.on_test_activate)
+        #~ self.keys.add_keybinding(key, 0)
+        self.keys.add_keybinding(GeanyGI.KeyBinding.new("test1", "Test1", self.on_test_activate), 0)
+        pass
 
     def do_version_check(self, abi):
         return 218
@@ -30,6 +42,7 @@ class PythonHelloPlugin2(GObject.Object, Geany.Plugin2):
     def do_cleanup(self):
         print("cleanup\n")
         self.doc.close()
+        self.keys = None
 
 def __init__():
     print("foo")
